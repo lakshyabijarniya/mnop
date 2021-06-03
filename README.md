8fa913c88874a9bac982a369258bbce4843bf044
Lakshya Kumar <77159568+lakshyabijarniya@users.noreply.github.com>
2021-06-03 11:17:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mnop
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
+
+var ground,paper,bottomSide,rightSide,leftSide;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 600);
+
+    rectMode(CENTER);
+	engine = Engine.create();
+	world = engine.world;
+	paper= new Paper(100,300,10,{isStatic:true});
+	ground=new Ground(600,580,1200,20,{isStatic:true});
+	dustbin=new Dustbin(1000,560,170,20); 
+    Launcher=new Launcher(paper.body,{x:200,y:20});
+
+}
+
+
+function draw() {
+
+
+   rectMode(CENTER);
+   background("blue");
+   Engine.update(engine);
+   paper.display();
+   ground.display();
+   dustbin.display();
+Launcher.display();
+
+
+}
+
+
+function mouseDragged(){
+    Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+    Launcher.fly();
+}
+
+
